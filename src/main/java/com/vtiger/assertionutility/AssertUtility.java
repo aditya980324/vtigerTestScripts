7ad47f1b91534f39ac4ae4cc252a1c8db246f955
aditya980324 <ag2881427@gmail.com>
2025-03-25 14:51:54 +0530

    Update AssertUtility.java

--- src/main/java/com/vtiger/assertionutility/AssertUtility.java
+++ src/main/java/com/vtiger/assertionutility/AssertUtility.java
@@ -3,5 +3,8 @@ package com.vtiger.assertionutility;
 import org.testng.ITestListener;
 
 public class AssertUtility {
+  public static void main(Strings[] args) {
+    System.out.println("Assert Function");
+  }
 
 }
