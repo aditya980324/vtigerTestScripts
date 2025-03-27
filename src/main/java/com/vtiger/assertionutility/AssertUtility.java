8aa9c2f8b0ea4f14df9a833a8f5cdc388bdc54db
Aditya goswami <ag2881427@gmail.com>
2025-03-27 16:26:19 +0530

    edit in github

--- src/main/java/com/vtiger/assertionutility/AssertUtility.java
+++ src/main/java/com/vtiger/assertionutility/AssertUtility.java
@@ -5,6 +5,7 @@ import org.testng.ITestListener;
 public class AssertUtility {
     public static void main(String[] args) {
         System.out.println("Assert Function");
+        System.out.println("AssertEqual Function");
     }
 
 }
